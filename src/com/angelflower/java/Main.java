340f7cef0cdc972aebf7f817e40718b68f3cbbee
AngelFlower <angelfc.contact@gmail.com>
2019-09-27 13:59:56 -0500

    Modal Dialog Password

--- src/com/angelflower/java/Main.java
+++ src/com/angelflower/java/Main.java
@@ -1,13 +1,14 @@
 package com.angelflower.java;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
-
 
 public class Main extends JFrame {
-    public static String SSID, MAC;
+    public static String SSID, MAC, Pass;
 
     public Main() {
         this.setTitle("GUP");
@@ -37,11 +38,6 @@ public class Main extends JFrame {
         txtMAC.setBounds(180,95,130,25);
         add(txtMAC);
 
-        JTextField txtPass = new JTextField();
-        txtPass.setBounds(130,170,130,25);
-        txtPass.setEditable(false);
-        add(txtPass);
-
         JButton btnGetPass = new JButton("Get Password");
         btnGetPass.setBounds(60,130,270,25);
         add(btnGetPass);
@@ -60,23 +56,68 @@ public class Main extends JFrame {
                 char[] ArrayMac = MAC.toCharArray();
                 char[] ArrayMacFiltered = FilterMAC(ArrayMac);
 
-                txtPass.setText(getPassword(ArrayMacFiltered, ArraySSIDFiltered));
-                System.out.println("Password: "+getPassword(ArrayMacFiltered, ArraySSIDFiltered));
+                Pass = getPassword(ArrayMacFiltered, ArraySSIDFiltered);
+
+                String[] options = new String[]{"Copy", "OK"};
+                JDialog JDshowPass = new JDialog();
+                JDshowPass.setSize(200, 200);
+                JDshowPass.setLocationRelativeTo(null);
+                JDshowPass.setLayout(null);
+                JDshowPass.setTitle(SSID+" - Ready");
+                JDshowPass.setModal(true);
+
+                JLabel textShowSSID = new JLabel("SSID: "+SSID);
+                textShowSSID.setBounds(30,25,100,25);
+                JDshowPass.add(textShowSSID);
+
+                JLabel textPass = new JLabel("Password: "+Pass);
+                textPass.setBounds(30,60,150,25);
+                JDshowPass.add(textPass);
+
+                JButton btnCopyPass = new JButton("Copy");
+                btnCopyPass.setBounds(10,130,84,25);
+                JDshowPass.add(btnCopyPass);
+                btnCopyPass.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        copyPass();
+                    }
+                });
+
+                JButton btnOK = new JButton("Ok");
+                btnOK.setBounds(93,130,80,25);
+                JDshowPass.add(btnOK);
+                btnOK.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JDshowPass.dispose();
+                    }
+                });
+
+                JDshowPass.setVisible(true);
+                System.out.println("Password: "+Pass);
             }
         });
 
         this.setVisible(true);
-
     }
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace ();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
         Main window = new Main();
-
-
-        //Scanner input = new Scanner(System.in);
-
-        //System.out.print("Enter SSID: ");
-
     }
 
     public static char[] FilterMAC(char[] ArrayMac){
@@ -112,5 +153,12 @@ public class Main extends JFrame {
         return String.copyValueOf(Pass);
     }
 
+    private void copyPass() {
+        Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection ss = new StringSelection(Pass);
+        System.out.println(Pass);
+        cb.setContents(ss, ss);
+
+    }
 
 }
